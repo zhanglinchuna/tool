c75609f583fd1b6e4710b147d3523348000000cf
zhanglinchuna <39194099+zhanglinchuna@users.noreply.github.com>
2020-09-07 10:19:56 +0800

    Update script.js

--- waterMark/script.js
+++ waterMark/script.js
@@ -2,7 +2,7 @@ var globaluserId='用户名';
 jQuery(document).ready(function () {
 	if(watermark){
 		var tem = window.setInterval(function () {
-			watermark.load({ watermark_txt: "安盛保险",userIdss:globaluserId });
+			watermark.load({ watermark_txt: "test666",userIdss:globaluserId });
 			clearInterval(tem);
 		},500);
 		PointerEventsPolyfill.initialize({});
